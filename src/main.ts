86c467fa698b8088b4dbbf7f5050ba680d25c805
Antonio Mortari <antoniomortari09@gmail.com>
2024-10-19 16:34:21 -0300

    modified cors options

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: ['https://debtflow-52771.web.app', 'http://localhost:4200']});
+  app.enableCors({
+    origin: ['https://debtflow-52771.web.app', 'http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
